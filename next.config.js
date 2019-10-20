c941a2952b12d02c6c76871244a0f3106916f6fb
Jarrod Flesch <jarrodmflesch@gmail.com>
2019-10-20 01:33:39 -0400

    adds env

--- next.config.js
+++ next.config.js
@@ -2,22 +2,23 @@ require('dotenv').config();
 const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
-	publicRuntimeConfig: {
-	},
-	sassLoaderOptions: {
-		includePaths: ['scss'],
-	},
-	webpack(config, options) {
-		config.module.rules.push({
-			test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-			use: {
-				loader: 'url-loader',
-				options: {
-					limit: 100000
-				}
-			}
-		});
+  publicRuntimeConfig: {
+    NODE_ENV: process.env.NODE_ENV,
+  },
+  sassLoaderOptions: {
+    includePaths: ['scss'],
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+        },
+      },
+    });
 
-		return config;
-	}
+    return config;
+  },
 });
